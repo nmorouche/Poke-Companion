94f99322d63d4989a608e4d795a5a66c08800e3c
Nassim Morouche <nassim.morouche-ext@kering.com>
2023-07-17 23:33:25 +0200

    Project created

---
+++ Poke-Companion/Poke-Companion/Poke_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_CompanionApp.swift
+//  Poke-Companion
+//
+//  Created by Nassim Morouche on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Poke_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
